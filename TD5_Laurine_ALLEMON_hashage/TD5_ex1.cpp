302bb96ab4638584b17c0696bd17fdeaeba0c3e7
Laurine <laurine.allemon@gmail.com>
2024-05-12 16:25:06 +0200

    TD5 exercice1 (fonction de hachage)
    question 1 et 2 faite (pas la 3)

--- TD5_Laurine_ALLEMON_hashage/TD5_ex1.cpp
+++ TD5_Laurine_ALLEMON_hashage/TD5_ex1.cpp
@@ -1,21 +1,18 @@
-// #include <iostream>
-// #include <vector>
-// #include <string>
+// // #include <iostream>
+// // #include <vector>
+// // #include <string>
 
-///////////////// TD5 ///////////
+// ///////////////// TD5 ///////////
 
-#include <iostream>
-#include <vector>
-#include <string>
-
-//EXERCICE 1
-
-//QUESTION 1
+// EXERCICE 1
+// QUESTION 1
 size_t folding_string_hash(std::string const &chaine, size_t max)
 {
     int somme{};
     size_t resultat{};
     int coeff{};
+    std::cout << std::endl;
+
     for (size_t i{0}; i < chaine.size(); i++)
     {
         std::cout << chaine[i] << " a pour valeur ascii : " << static_cast<int>(chaine[i]) << std::endl;
@@ -32,19 +29,63 @@ size_t folding_string_hash(std::string const &chaine, size_t max)
     else
     {
         resultat = somme;
-        std::cout << "La somme de toutes les valeurs ascii est : ";
+        std::cout << "---- La somme de toutes les valeurs ascii est : ";
+        return resultat;
+    }
+}
+
+// QUESTION 2
+size_t folding_string_ordered_hash(std::string const &chaine, size_t max)
+{
+    int somme{};
+    size_t resultat{};
+    int coeff{};
+    std::cout << std::endl;
+
+    for (size_t i{0}; i < chaine.size(); i++)
+    {
+        int mult{static_cast<int>(chaine[i]) * static_cast<int>(i)};
+        std::cout << chaine[i] << " a pour valeur ascii : " << static_cast<int>(chaine[i]) << std::endl;
+        std::cout << "on multiplie la valeur ascii par sa position dans la chaine de caractere, ce qui donne : " << mult << std::endl;
+        somme += mult;
+    }
+
+    if (somme > max)
+    {
+        coeff = somme / max;
+        resultat = somme - (coeff * max);
+        std::cout << " ---- En appliquant le modulo par rapport a \"max\" sur la somme, on obtient une somme egale a : ";
+        return resultat;
+    }
+    else
+    {
+        resultat = somme;
+        std::cout << " ---- La somme de toutes les valeurs ascii est : ";
         return resultat;
     }
 }
 
 int main()
 {
-    std::string const &mot{"bonjour"};
-    // size_t max{mot.size()};
-    std::cout << folding_string_hash(mot, 1024);
+    std::string const &mot1{"salut"};
+    std::string const &mot2{"bonjour"};
+    std::string const &mot3{"jourbon"};
+
+    std::cout << "__________________________________________________________________________" << std::endl;
+    std::cout << "---------------  EX 1 -- quest 1 -- mot1 ------------" << std::endl;
+    std::cout << "__________________________________________________________________________" << std::endl;
+    std::cout << "POUR \"SALUT\" : " << folding_string_hash(mot1, 1024);
+    std::cout << std::endl;
+
+    std::cout << "__________________________________________________________________________" << std::endl;
+    std::cout << "---------------  EX 1 -- quest 2 -- mot2 ------------" << std::endl;
+    std::cout << "__________________________________________________________________________" << std::endl;
+    std::cout << "POUR \"BONJOUR\" : " << folding_string_ordered_hash(mot2, 1024) << std::endl; //Le valeur hachée de \"bonjour\" est : "
+
+    std::cout << "__________________________________________________________________________" << std::endl;
+    std::cout << "--------------- EX 1 -- quest 2 -- mot3  --------------- " << std::endl;
+    std::cout << "__________________________________________________________________________" << std::endl;
+    std::cout << "POUR \"JOURBON\" : " << folding_string_ordered_hash(mot3, 1024) << std::endl; //Le valeur hachée de \"jourbon\" est : "
 
     return 0;
 }
-
-//QUESITON 2
-// size_t folding_string_ordered_hash(std::string const &s, size_t max);
